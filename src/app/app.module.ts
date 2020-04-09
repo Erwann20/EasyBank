dd4d35fd093f3321d3a2cdbe29ff1e051220f2d9
Erwann20 <erwann.bret@gmail.com>
2020-04-09 11:35:26 +0200

    init project & fix style with bulma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HeroComponent } from './hero/hero.component';
+import { ButtonComponent } from './button/button.component';
+import { AboutComponent } from './about/about.component';
+import { SimpleCardComponent } from './simple-card/simple-card.component';
+import { LatestArticlesComponent } from './latest-articles/latest-articles.component';
+import { ImageCardComponent } from './image-card/image-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeroComponent,
+    ButtonComponent,
+    AboutComponent,
+    SimpleCardComponent,
+    LatestArticlesComponent,
+    ImageCardComponent
   ],
   imports: [
     BrowserModule
